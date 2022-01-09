9b2381c581098f937d30d362cc8f94a18ba3e55b
Mateusz Harnik <36229431+mateuszharnik@users.noreply.github.com>
2022-01-09 14:18:55 +0100

    :sparkles: [RB-14] Page 404
    Create 404 page. Move navigation to the Main component.
    Change styles for link.

--- src/client/App/index.jsx
+++ src/client/App/index.jsx
@@ -1,50 +1,13 @@
 import React, { memo } from 'react';
-import { Link } from 'react-router-dom';
 import { useStoreState } from 'easy-peasy';
 import Router from '@client/router.js';
 
 const App = memo(() => {
+  // eslint-disable-next-line no-unused-vars
   const message = useStoreState(({ app }) => app.message);
 
   return (
-    <div>
-      {message && <div>{message}</div>}
-      <nav>
-        <ul className="nav">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">
-              Strona główna
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/o-nas">
-              O nas
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/kontakt">
-              Kontakt
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/najczesciej-zadawane-pytania">
-              FAQ
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/zaloguj">
-              Logowanie
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/404">
-              404
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <Router />
-    </div>
+    <Router />
   );
 });
 
--- src/client/views/Webpage/Main/index.jsx
+++ src/client/views/Webpage/Main/index.jsx
@@ -1,5 +1,5 @@
 import React, { memo, useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 import logo from '@client/assets/images/logo-dark.svg';
 
 const Main = memo(() => {
@@ -18,22 +18,58 @@ const Main = memo(() => {
   };
 
   return (
-    <div className="container text-center mt-5">
-      <img src={logo} width="100" height="55" className="img-fluid" alt="Logo strony" />
-      <h2 className="mt-5 mb-10">Naciśnij przycisk, żeby wyświetlić wiadomość.</h2>
-      <button
-        type="button"
-        className="btn btn-primary rounded-pill"
-        title="Pokaż"
-        onClick={handleClick}
-      >
-        Pokaż wiadomość
-      </button>
-      {message && <p className="mt-5">{message}</p>}
-      <div className="mt-4">
-        <Outlet />
+    <>
+      <nav>
+        <ul className="nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">
+              Strona główna
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/o-nas">
+              O nas
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/kontakt">
+              Kontakt
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/najczesciej-zadawane-pytania">
+              FAQ
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/zaloguj">
+              Logowanie
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/404">
+              404
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="container text-center mt-5">
+        <img src={logo} width="100" height="55" className="img-fluid" alt="Logo strony" />
+        <h2 className="mt-5 mb-10">Naciśnij przycisk, żeby wyświetlić wiadomość.</h2>
+        <button
+          type="button"
+          className="btn btn-primary rounded-pill"
+          title="Pokaż"
+          onClick={handleClick}
+        >
+          Pokaż wiadomość
+        </button>
+        {message && <p className="mt-5">{message}</p>}
+        <div className="mt-4">
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </>
   );
 });
 
