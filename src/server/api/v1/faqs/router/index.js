523256c0b1efa308ada8ba7c80e10c2b4e3bc32d
Mateusz Harnik <36229431+mateuszharnik@users.noreply.github.com>
2022-04-09 12:05:56 +0200

    :book: [RB-94] Update frequently asked question
    Create endpoint for updating frequently asked question.
    Create endpoint for publishing frequently asked question.

--- src/server/api/v1/faqs/router/index.js
+++ src/server/api/v1/faqs/router/index.js
@@ -3,6 +3,8 @@ import manage from '@server/helpers/roles';
 import { isLoggedIn, canManage } from '@server/middlewares/auth';
 import {
   createFAQ,
+  updateFAQ,
+  toggleIsPublishedFAQ,
 } from '../controller';
 
 const router = Router();
@@ -12,6 +14,20 @@ router.post(
   isLoggedIn,
   canManage(manage.faqs),
   createFAQ,
+);
+
+router.patch(
+  '/:id/publish',
+  isLoggedIn,
+  canManage(manage.faqs),
+  toggleIsPublishedFAQ,
+);
+
+router.patch(
+  '/:id',
+  isLoggedIn,
+  canManage(manage.faqs),
+  updateFAQ,
 );
 
 export default router;
