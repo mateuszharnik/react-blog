1cc5df2503b97aadf5026d75d22c3c4181f87685
Mateusz Harnik <36229431+mateuszharnik@users.noreply.github.com>
2022-07-07 21:06:12 +0200

    a

--- src/server/app/index.test.js
+++ src/server/app/index.test.js
@@ -8,8 +8,16 @@ describe('App', () => {
   });
 
   it('should response with status code 200', async () => {
-    const response = await request(app).get('/api/v1');
+    const response = await request(app).get('/api/v1/about');
 
     expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual(expect.objectContaining({
+      contents: '',
+      html_contents: '',
+      created_at: expect.any(String),
+      updated_at: expect.any(String),
+      deleted_at: null,
+      id: expect.any(String),
+    }));
   });
 });
