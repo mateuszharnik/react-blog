796fd3fc2cf3b9ec238b817f5e8b54ac0a40ac88
Mateusz Harnik <36229431+mateuszharnik@users.noreply.github.com>
2022-07-08 23:45:30 +0200

    A

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,7 @@ import config from '@server/config';
 import app from '@server/app';
 
 const { PORT } = config;
+// eslint-disable-next-line
 console.log(process.env.NODE_ENV);
 
 // eslint-disable-next-line
