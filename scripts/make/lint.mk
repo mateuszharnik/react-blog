493f7c42494801ae0afdee52cd3cd02467316f7c
Mateusz Harnik <36229431+mateuszharnik@users.noreply.github.com>
2022-12-31 09:29:14 +0100

    :book: [RB-132] Linter for json files
    Add linter for json files. Update github actions.

--- scripts/make/lint.mk
+++ scripts/make/lint.mk
@@ -11,22 +11,22 @@ lint:
 .ONESHELL:
 lint@js: # Lint all js and jsx files
 lint@js:
-	@cross-env NODE_ENV=production npx eslint ./src/ --ext .js,.jsx
+	@cross-env NODE_ENV=production npx eslint ./src/ --ext .js,.jsx,.json
 
 .ONESHELL:
 lint@server: # Lint all js files in server directory
 lint@server:
-	@cross-env NODE_ENV=production npx eslint ./src/server/ --ext .js
+	@cross-env NODE_ENV=production npx eslint ./src/server/ --ext .js,.json
 
 .ONESHELL:
 lint@client: # Lint all js and jsx files in client directory
 lint@client:
-	@cross-env NODE_ENV=production npx eslint ./src/client/ --ext .js,.jsx
+	@cross-env NODE_ENV=production npx eslint ./src/client/ --ext .js,.jsx,.json
 
 .ONESHELL:
 lint@e2e: # Lint all js files in e2e directory
 lint@e2e:
-	@cross-env NODE_ENV=production npx eslint ./src/e2e/ --ext .js
+	@cross-env NODE_ENV=production npx eslint ./src/e2e/ --ext .js,.json
 
 .ONESHELL:
 lint@shared: # Lint all js files in shared directory
@@ -61,22 +61,22 @@ lint-fix:
 .ONESHELL:
 lint-fix@js: # Lint and fix all js and jsx files
 lint-fix@js:
-	@cross-env NODE_ENV=production npx eslint ./src/ --ext .js,.jsx --fix
+	@cross-env NODE_ENV=production npx eslint ./src/ --ext .js,.jsx,.json --fix
 
 .ONESHELL:
 lint-fix@server: # Lint and fix all js files in server directory
 lint-fix@server:
-	@cross-env NODE_ENV=production npx eslint ./src/server/ --ext .js --fix
+	@cross-env NODE_ENV=production npx eslint ./src/server/ --ext .js,.json --fix
 
 .ONESHELL:
 lint-fix@client: # Lint and fix all js and jsx files in client directory
 lint-fix@client:
-	@cross-env NODE_ENV=production npx eslint ./src/client/ --ext .js,.jsx --fix
+	@cross-env NODE_ENV=production npx eslint ./src/client/ --ext .js,.jsx,.json --fix
 
 .ONESHELL:
 lint-fix@e2e: # Lint and fix all js files in e2e directory
 lint-fix@e2e:
-	@cross-env NODE_ENV=production npx eslint ./src/e2e/ --ext .js --fix
+	@cross-env NODE_ENV=production npx eslint ./src/e2e/ --ext .js,.json --fix
 
 .ONESHELL:
 lint-fix@shared: # Lint and fix all js files in shared directory
