1f1fdfe2af1650eb4719ff286e7c0dae8d37f63c
Mateusz Harnik <36229431+mateuszharnik@users.noreply.github.com>
2023-04-22 13:18:15 +0200

    Refactor hooks

---
+++ src/client/hooks/useHead/useHead.js
@@ -0,0 +1,53 @@
+import { useEffect } from 'react';
+
+export const useHead = ({
+  title = '',
+  description = '',
+  keywords = '',
+  imageAlt = '',
+  siteName = '',
+}) => {
+  useEffect(() => {
+    const trimmedSiteName = siteName.trim();
+
+    if (trimmedSiteName) {
+      document.querySelector('meta[property="og:site_name"]')?.setAttribute('content', trimmedSiteName);
+    }
+  }, [siteName]);
+
+  useEffect(() => {
+    const trimmedTitle = title.trim();
+
+    if (trimmedTitle) {
+      document.title = trimmedTitle;
+      document.querySelector('meta[property="og:title"]')?.setAttribute('content', trimmedTitle);
+      document.querySelector('meta[name="twitter:title"]')?.setAttribute('content', trimmedTitle);
+    }
+  }, [title]);
+
+  useEffect(() => {
+    const trimmedDescription = description.trim();
+
+    if (trimmedDescription) {
+      document.querySelector('meta[name="description"]')?.setAttribute('content', trimmedDescription);
+      document.querySelector('meta[property="og:description"]')?.setAttribute('content', trimmedDescription);
+      document.querySelector('meta[name="twitter:description"]')?.setAttribute('content', trimmedDescription);
+    }
+  }, [description]);
+
+  useEffect(() => {
+    const trimmedImageAlt = imageAlt.trim();
+
+    if (trimmedImageAlt) {
+      document.querySelector('meta[property="og:image:alt"]')?.setAttribute('content', trimmedImageAlt);
+    }
+  }, [imageAlt]);
+
+  useEffect(() => {
+    const trimmedKeywords = keywords.trim();
+
+    if (trimmedKeywords) {
+      document.querySelector('meta[name="keywords"]')?.setAttribute('content', trimmedKeywords);
+    }
+  }, [keywords]);
+};
