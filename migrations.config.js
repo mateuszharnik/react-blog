44b6dc910269a0cd6346e431684ae99ac7f8684b
Mateusz Harnik <36229431+mateuszharnik@users.noreply.github.com>
2024-06-30 13:03:01 +0200

    :bug: [RB-186] Fix

--- migrations.config.js
+++ migrations.config.js
@@ -13,8 +13,6 @@ const path = resolve(process.cwd(), `.env.${process.env.APP_ENV}`);
 
 if (existsSync(path)) config({ path });
 
-console.log({ TESTOWANIE: process.env.RATE_LIMIT });
-
 module.exports = {
   mongodb: {
     url: process.env.DB_URL,
